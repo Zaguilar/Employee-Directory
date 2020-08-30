d283328ef38e58c6bfeb547d38ede532e15fcbf4
Zachary Aguilar <zacharyjag@yahoo.com>
2020-08-29 22:23:51 -0500

    updated

--- src/components/EmployeeCard/index.js
+++ src/components/EmployeeCard/index.js
@@ -14,24 +14,23 @@ function EmployeeCard(props) {
           <div className="col-8 col-md-10">
             <div className="row">
               <div className="col-12 col-md-6 col-xl-3">
-                <strong className='text-warning'>Name:</strong> {props.firstName} {props.lastName}
+                <strong className='text-primary'>Name:</strong> {props.firstName} {props.lastName}
               </div>
               <div className="col-12 col-md-6 col-xl-3">
-                <div className=""><strong className='text-warning'>Location:</strong> {props.location}</div>
+                <div className=""><strong className='text-primary'>Location:</strong> {props.location}</div>
               </div>
 
               <div className="col-12 col-md-6 col-xl-3">
-                <strong className='text-warning'>Role:</strong> {props.role}
+                <strong className='text-primary'>Role:</strong> {props.role}
               </div>
               <div className="col-12 col-md-6 col-xl-3">
-                <strong className='text-warning'>Department:</strong> {props.department}
+                <strong className='text-primary'>Department:</strong> {props.department}
               </div>
             </div>
 
           </div>
         </div>
       </div>
-      {/* <span className="remove" onClick={() => props.handleRemoveFriend(props.id)}>𝘅</span> */}
     </div>
   );
 }
